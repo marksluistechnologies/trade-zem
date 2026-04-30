a1fd4c5b700055dceae3f1a4338514055b9b42d8
help-haramexports <help.haramexports@gmail.com>
2026-04-30 11:45:03 +0530

    Fixed missing useState import in Dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { Canvas } from '@react-three/fiber';
 import { Stars, Sparkles } from '@react-three/drei';
